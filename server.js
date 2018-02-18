940fe9a673d643872331103caeed2f6a74caf088
prabhat963 <36572969+prabhat963@users.noreply.github.com>
2018-02-18 18:22:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res) {
-  res.send(createtemplate(articles[articlesName]));  
+  res.send(createtemplate(articles[articleName]));  
 });
 app.get('/article-two', function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
