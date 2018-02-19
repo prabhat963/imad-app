d94dcdd264c07f335a870692b235b18c3083031a
prabhat963 <36572969+prabhat963@users.noreply.github.com>
2018-02-19 23:07:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function(req,res){
    counter=counter+1;
-   res.send(counter,toString());
+   res.send(counter.toString());
 });
 
 app.get('/article-one', function(req,res) {
