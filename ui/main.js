08f56bc6ab4ac52849df00fe8e96a8bcbeea12ad
prabhat963 <36572969+prabhat963@users.noreply.github.com>
2018-02-20 18:55:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ var button = document.getElementById('counter');
                 if(request.status === 200)
                  {
                     var counter = request.responseText;
-                     var span = document.getElementById('count');
+                     var span = document.getElementById('counter');
                      span.innerHTML = counter.toString();
                 }
          }
